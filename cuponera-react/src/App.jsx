1b1ef97e0800fc721b754b46929c788d97f0c84a
XMauriX001 <mauriciorecinos021176@gmail.com>
2026-02-17 18:16:14 -0600

    Update App.jsx
    Incluir una ruta momentanea

--- cuponera-react/src/App.jsx
+++ cuponera-react/src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Home from './components/Home'
 
 function App() {
   return (
-    <BrowserRouter></BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
