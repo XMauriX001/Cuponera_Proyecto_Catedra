465a95a4862dc1f8c9f81ccb9bbc6eefddb060b2
XMauriX001 <mauriciorecinos021176@gmail.com>
2026-02-20 18:53:55 -0600

    Update OfertaSeeder.php
    Nuevos datos para el seeder

--- cuponera-api/database/seeders/OfertaSeeder.php
+++ cuponera-api/database/seeders/OfertaSeeder.php
@@ -13,7 +13,7 @@ class OfertaSeeder extends Seeder
             // Pizza Hut (Restaurantes = rubro_id 1)
             [
                 'empresa_id' => 1,
-                'rubro_id' => 1,  // <-- AGREGA ESTO
+                'rubro_id' => 1,
                 'titulo' => '2x1 en Pizzas Medianas',
                 'precio_regular' => 25.00,
                 'precio_oferta' => 15.00,
@@ -27,7 +27,7 @@ class OfertaSeeder extends Seeder
             ],
             [
                 'empresa_id' => 1,
-                'rubro_id' => 1,  // <-- AGREGA ESTO
+                'rubro_id' => 1,
                 'titulo' => 'Combo Familiar',
                 'precio_regular' => 35.00,
                 'precio_oferta' => 22.00,
@@ -39,10 +39,25 @@ class OfertaSeeder extends Seeder
                 'otros_detalles' => 'Disponible todos los días',
                 'estado' => 'aprobada',
             ],
+            [
+                'empresa_id' => 1,
+                'rubro_id' => 1,
+                'titulo' => 'Combo Personal',
+                'precio_regular' => 10.00,
+                'precio_oferta' => 4.99,
+                'fecha_inicio' => '2026-02-01',
+                'fecha_fin' => '2026-03-01',
+                'fecha_limite_cupon' => '2026-03-15',
+                'cantidad_limite' => 150,
+                'descripcion' => '1 Pizza personal + bebida + acompañamiento',
+                'otros_detalles' => 'Ideal para una persona',
+                'estado' => 'aprobada',
+            ],
+
             // Salon Glamour (Salones de Belleza = rubro_id 2)
             [
                 'empresa_id' => 2,
-                'rubro_id' => 2,  // <-- AGREGA ESTO
+                'rubro_id' => 2,
                 'titulo' => 'Corte + Tinte',
                 'precio_regular' => 80.00,
                 'precio_oferta' => 45.00,
@@ -56,7 +71,7 @@ class OfertaSeeder extends Seeder
             ],
             [
                 'empresa_id' => 2,
-                'rubro_id' => 2,  // <-- AGREGA ESTO
+                'rubro_id' => 2,
                 'titulo' => 'Manicure + Pedicure',
                 'precio_regular' => 30.00,
                 'precio_oferta' => 18.00,
@@ -68,10 +83,25 @@ class OfertaSeeder extends Seeder
                 'otros_detalles' => 'Incluye esmaltado',
                 'estado' => 'aprobada',
             ],
+            [
+                'empresa_id' => 2,
+                'rubro_id' => 2,
+                'titulo' => 'Alisado Permanente + Corte',
+                'precio_regular' => 60.00,
+                'precio_oferta' => 29.99,
+                'fecha_inicio' => '2026-02-01',
+                'fecha_fin' => '2026-03-05',
+                'fecha_limite_cupon' => '2026-03-20',
+                'cantidad_limite' => 40,
+                'descripcion' => 'Tratamiento completo de hidratación y corte de puntas',
+                'otros_detalles' => 'Incluye productos profesionales',
+                'estado' => 'aprobada',
+            ],
+
             // Cinemark (Entretenimiento = rubro_id 3)
             [
                 'empresa_id' => 3,
-                'rubro_id' => 3,  // <-- AGREGA ESTO
+                'rubro_id' => 3,
                 'titulo' => '2 Boletos + Palomitas',
                 'precio_regular' => 18.00,
                 'precio_oferta' => 12.00,
@@ -81,6 +111,34 @@ class OfertaSeeder extends Seeder
                 'cantidad_limite' => 200,
                 'descripcion' => '2 entradas de cine + 1 palomitas mediana',
                 'otros_detalles' => 'Válido de lunes a jueves',
+                'estado' => 'aprobada',
+            ],
+            [
+                'empresa_id' => 3,
+                'rubro_id' => 3,
+                'titulo' => '4 Boletos + Combo Familiar',
+                'precio_regular' => 40.00,
+                'precio_oferta' => 25.00,
+                'fecha_inicio' => '2026-02-01',
+                'fecha_fin' => '2026-03-10',
+                'fecha_limite_cupon' => '2026-03-20',
+                'cantidad_limite' => 100,
+                'descripcion' => 'Entradas válidas para cualquier función 2D + combo gigante',
+                'otros_detalles' => 'Incluye 2 palomitas grandes y 4 bebidas',
+                'estado' => 'aprobada',
+            ],
+            [
+                'empresa_id' => 3,
+                'rubro_id' => 3,
+                'titulo' => 'Pase VIP Todo el Día',
+                'precio_regular' => 30.00,
+                'precio_oferta' => 15.00,
+                'fecha_inicio' => '2026-02-01',
+                'fecha_fin' => '2026-03-12',
+                'fecha_limite_cupon' => '2026-03-25',
+                'cantidad_limite' => 75,
+                'descripcion' => 'Acceso ilimitado a juegos mecánicos y área de comida',
+                'otros_detalles' => 'No incluye juegos de habilidad',
                 'estado' => 'aprobada',
             ],
         ];
